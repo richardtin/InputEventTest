be0f084f3b292bc94f5405920b20cbfc239c5280
Richard.CW.Ting <richard.cw.ting@benq.com>
2020-12-25 11:23:06 +0800

    feat(app): add a drawing view to main screen

--- app/src/main/java/io/github/richardtin/inputeventtest/MainActivity.kt
+++ app/src/main/java/io/github/richardtin/inputeventtest/MainActivity.kt
@@ -1,11 +1,16 @@
 package io.github.richardtin.inputeventtest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import io.github.richardtin.inputeventtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
