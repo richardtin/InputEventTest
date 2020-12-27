98cba2b976b020d3537cc5f705c843d0cefc6d2d
Richard Ting <richard.tin@gmail.com>
2020-12-27 10:26:12 +0800

    feat(app): tuning UI

--- app/src/main/java/io/github/richardtin/inputeventtest/MainActivity.kt
+++ app/src/main/java/io/github/richardtin/inputeventtest/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.databinding.DataBindingUtil
 import io.github.richardtin.inputeventtest.databinding.ActivityMainBinding
@@ -14,6 +15,7 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     lateinit var mainBinding: ActivityMainBinding
+    var logName: String? = null
     var logPath: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,42 +23,52 @@ class MainActivity : AppCompatActivity() {
         mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         mainBinding.run {
-            loggerStart.setOnClickListener {
-                logfileName.text = generateLogfileName()
-                logPath = "${getSavedDir()}/${logfileName.text}"
-                drawingView.logger = InputEventLogger(logPath!!)
-                appHint.visibility = View.GONE
-                penTypeOptionThin.isEnabled = false
-                penTypeOptionThick.isEnabled = false
-                save.isEnabled = false
-            }
-            loggerStop.setOnClickListener {
-                drawingView.clear()
-                drawingView.logger?.close()
-                drawingView.logger = null
-                appHint.visibility = View.VISIBLE
-                penTypeOptionThin.isEnabled = true
-                penTypeOptionThick.isEnabled = true
-                save.isEnabled = true
-            }
-        }
-        mainBinding.save.setOnClickListener {
-            logPath?.let {
-                val fileUri =
-                    FileProvider.getUriForFile(this, "$packageName.fileprovider", File(it))
-                val shareIntent: Intent = Intent().apply {
-                    action = Intent.ACTION_SEND
-                    putExtra(Intent.EXTRA_STREAM, fileUri)
-                    type = "text/plain"
+            recorderState.addOnButtonCheckedListener { _, _, isChecked ->
+                penTypeOptionThin.isEnabled = !isChecked
+                penTypeOptionThick.isEnabled = !isChecked
+                save.isEnabled = !isChecked
+
+                if (isChecked) {
+                    // Logger
+                    startInputEventLogger()
+
+                    // UI
+                    appHint.visibility = View.GONE
+                    recorder.icon = ContextCompat.getDrawable(this@MainActivity, R.drawable.ic_stop_circle)
+                    logfileName.text = logName
+                } else {
+                    stopInputEventLogger()
+
+                    // UI
+                    drawingView.clear()
+                    appHint.visibility = View.VISIBLE
+                    recorder.icon = ContextCompat.getDrawable(this@MainActivity, R.drawable.ic_play_circle)
+                    sendLog()
                 }
-                startActivity(Intent.createChooser(shareIntent, null))
+            }
+            save.setOnClickListener {
+                sendLog()
             }
         }
     }
 
     override fun onDestroy() {
+        if (mainBinding.recorder.isChecked) {
+            stopInputEventLogger()
+        }
         deleteAllInFolder(filesDir)
         super.onDestroy()
+    }
+
+    private fun startInputEventLogger() {
+        logName = generateLogName()
+        logPath = "${getSavedDir()}/${logName}"
+        mainBinding.drawingView.logger = InputEventLogger(logPath!!)
+    }
+
+    private fun stopInputEventLogger() {
+        mainBinding.drawingView.logger?.close()
+        mainBinding.drawingView.logger = null
     }
 
     private fun deleteAllInFolder(fileOrDirectory: File) {
@@ -89,7 +101,21 @@ class MainActivity : AppCompatActivity() {
         return dateFormatter.format(getCurrentDate())
     }
 
-    private fun generateLogfileName(): String {
+    private fun generateLogName(): String {
         return "${getCurrentDateString()}_${getPenGroupCheckedItem()}.csv"
     }
+
+    private fun sendLog() {
+        logPath?.let {
+            val fileUri =
+                FileProvider.getUriForFile(this, "$packageName.fileprovider", File(it))
+            val shareIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_STREAM, fileUri)
+                type = "text/plain"
+            }
+            startActivity(Intent.createChooser(shareIntent, null))
+        }
+    }
+
 }
